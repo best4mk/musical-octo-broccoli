7122c1ab40336f6ee17b1b5c5b22f2ec51b18009
Mari <best4mk@gmail.com>
2022-07-07 17:11:20 +0300

    fix code style

--- src/app/products/product-card/product-card.component.ts
+++ src/app/products/product-card/product-card.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Product } from 'src/app/types';
 
-
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
